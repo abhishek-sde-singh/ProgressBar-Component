6bac1648975f2b6d4923deabadb581a36672679b
Abhishek Singh <abhi@Abhisheks-MacBook-Pro.local>
2024-10-08 23:26:16 +0530

    heading change

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import ProgressBar from "./ProgressBar";
 function App() {
   return (
     <div className="flex flex-col justify-center items-center h-[100vh] w-full bg-gray-500">
-      <h1 className="text-5xl text-teal-300">Star Rating System</h1>
+      <h1 className="text-5xl text-teal-300">Progress Bar</h1>
       <ProgressBar />
     </div>
   );
